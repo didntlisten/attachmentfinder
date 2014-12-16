5c893dd1b40c2c18f4018e36ab58c17af0ca038f
Adam Lobb <alobb@zendesk.com>
2014-12-16 11:37:53 -0800

    remove debug statements

--- app.js
+++ app.js
@@ -12,8 +12,6 @@
 
       var setting2 = this.setting('Previous Attachments Tag');
 
-      console.log(setting2);
- 
       ticket.comments().forEach(function(comment) { // Checks all but current comment for attachments
         var firstImageAttachment = comment.imageAttachments().get(0);
         var firstNonImageAttachment = comment.nonImageAttachments()[0];
@@ -25,8 +23,6 @@
       });
  
       var setting1 = this.setting('Current Attachments Tag');
- 
-      console.log(setting1);
  
       if (comment.attachments().length > 0) { // Checks current comment for attachments
         ticket.tags().add(setting1);
